538ae43fe8c42ce14d3bbeac7bc1445395be12e9
Shivanshu11910886 <136676082+Shivanshu11910886@users.noreply.github.com>
2023-07-03 16:21:52 +0530

    Updates

---
+++ precache-manifest.2d14762fd1a3b7e73c7a15bac495f498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "98c5bd1f6b32a9659f2e",
+    "url": "/Text-utils/static/css/main.67f9c59f.chunk.css"
+  },
+  {
+    "revision": "98c5bd1f6b32a9659f2e",
+    "url": "/Text-utils/static/js/main.b6bcebd2.chunk.js"
+  },
+  {
+    "revision": "10cbae0b33da3880ec98",
+    "url": "/Text-utils/static/js/runtime~main.2df052ee.js"
+  },
+  {
+    "revision": "9a9707ffc9a823fc1f21",
+    "url": "/Text-utils/static/js/2.12692025.chunk.js"
+  },
+  {
+    "revision": "15a50c1c2b2805fa91df",
+    "url": "/Text-utils/static/js/3.e92f44bb.chunk.js"
+  },
+  {
+    "revision": "39d2bffa4ac9baa01d216cce1c7227be",
+    "url": "/Text-utils/index.html"
+  }
+];
